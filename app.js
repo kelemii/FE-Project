597efb3618462d18239e82fecbcd31e7a10a27d6
kelemii <98933496+kelemii@users.noreply.github.com>
2022-07-07 10:22:52 -0700

    Search function added, cards display in row & column format

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 const $userForm = $("#user-form");
 const $button = $(".click");
 const $container = "#results";
+const $userForm1 = $("#user-form1");
 
+// RANDOM BUTTON
 $userForm.submit((event) => {
   event.preventDefault();
   $('#results').html("");
@@ -42,3 +44,53 @@ $userForm.submit((event) => {
       .prependTo($div);
   });
 });
+
+
+const $input = $('input[name="search"]')
+
+//Search
+$userForm1.submit((event) => {
+  const userInput = $input.val();
+  event.preventDefault();
+  $('#results').html("");
+  const URL = `https://rickandmortyapi.com/api/character/?name=${userInput}`;
+  $.get(URL, (data) => {
+    console.log(data);
+    for (let i=0;i<data.results.length;i++){
+      let current = data.results[i]
+      const $div = $(`<div></div>`).addClass("card-div").prependTo($container);
+      if (current.status === "Alive") {
+        const $cardStatus = $(
+          `<h5><button class="alive"></button><em>  ${current.status}</em</h5>`
+        )
+          .addClass("card-status")
+          .prependTo($div);
+      } else if (current.status === "Dead") {
+        const $cardStatus = $(
+          `<h5><button class="dead"></button><em>  ${current.status}</em></h5>`
+        )
+          .addClass("card-status")
+          .prependTo($div);
+      } else if (current.status === "unknown") {
+        const $cardStatus = $(
+          `<h5><button class="unknown"></button><em>  ${current.status}</em></h5>`
+        )
+          .addClass("card-status")
+          .prependTo($div);
+      }
+      const $species = $(`<p>Species: ${current.species}</p>`)
+        .addClass("card-species")
+        .prependTo($div);
+      const $location = $(`<p>Location: ${current.location.name}</p>`)
+        .addClass("card-location")
+        .prependTo($div);
+      const $cardTitle = $(`<h3>${current.name}</h3>`)
+        .addClass("card-title")
+        .prependTo($div);
+      const $img = $(`<img src='${current.image}'>`)
+        .addClass("card-image")
+        .prependTo($div);
+    }
+  });
+});
+
